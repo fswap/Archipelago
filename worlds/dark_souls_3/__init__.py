f32afdc3ac9aa2946d6ccec9c78cab0d3a7fbc99
Szabó Benedek Zoltán <99742992+TVV1GK@users.noreply.github.com>
2026-06-12 23:53:28 +0200

    Remove duplicate rule

--- worlds/dark_souls_3/__init__.py
+++ worlds/dark_souls_3/__init__.py
@@ -1062,15 +1062,6 @@ class DarkSouls3World(World):
             and state.has("Izalith Pyromancy Tome", self.player)
         ))
 
-        self._add_location_rule([
-            "US: Old Sage's Blindfold - kill Cornyx", "US: Cornyx's Garb - kill Cornyx",
-            "US: Cornyx's Wrap - kill Cornyx", "US: Cornyx's Skirt - kill Cornyx"
-        ], lambda state: (
-            state.has("Great Swamp Pyromancy Tome", self.player)
-            and state.has("Carthus Pyromancy Tome", self.player)
-            and state.has("Izalith Pyromancy Tome", self.player)
-        ))
-
         ## Irina
 
         self._add_location_rule([
